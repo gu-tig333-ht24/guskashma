dd37804f3d39672c0575afcff38b0b6d50ba7de1
Mays525 <mays.kashko@icloud.com>
2024-10-17 13:58:15 +0200

    Separated and fixed the classes.

--- lib/api.dart
+++ lib/api.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
-import './main.dart';
+import 'new_class.dart'; 
 import 'dart:convert';
 
 Future<void> addToServer(String uppdrag) async {
@@ -13,16 +13,21 @@ Future<void> addToServer(String uppdrag) async {
 Future<List<Todo>> getFromServer() async {
   final http.Response response = await http.get(Uri.parse(
       "https://todoapp-api.apps.k8s.gu.se/todos?key=59f21635-eb90-491e-8439-5a68c83a72c9"));
-  List<dynamic> listDeco = jsonDecode(response.body);
-  return listDeco.map((item) => Todo.fromJson(item)).toList();
+  
+  if (response.statusCode == 200) {
+    List<dynamic> listDeco = jsonDecode(response.body);
+    return listDeco.map((item) => Todo.fromJson(item)).toList(); // Use TodoModel here
+  } else {
+    throw Exception('Failed to load todos from server');
+  }
 }
 
-Future<void> deleteFromserver(String id) async {
+Future<void> deleteFromServer(String id) async {
   await http.delete(Uri.parse(
       "https://todoapp-api.apps.k8s.gu.se/todos/$id?key=59f21635-eb90-491e-8439-5a68c83a72c9"));
 }
 
-Future<void> uppdateServer(bool check, String id, String title) async {
+Future<void> updateServer(bool check, String id, String title) async {
   await http.put(
     Uri.parse(
         "https://todoapp-api.apps.k8s.gu.se/todos/$id?key=59f21635-eb90-491e-8439-5a68c83a72c9"),
