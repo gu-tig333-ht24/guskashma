76fff6da911e4df4d2e29f0e3bf67f79b4d97873
Mays525 <mays.kashko@icloud.com>
2024-10-21 09:49:41 +0200

    Separated and fixed the classes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import './new_class.dart';
+import './todo_list_model.dart';
 import './add_to_list.dart';
-import './api.dart';
 
 void main() {
-  Todo state = Todo("", false, "");
   runApp(ChangeNotifierProvider(
-    create: (context) => state,
+    create: (context) => TodoListModel(),
     child: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ToDoList(), 
+      home: To_DO_List(),
     );
   }
 }
 
-
-class ToDoList extends StatefulWidget {
-  const ToDoList({super.key});
-
+class To_DO_List extends StatefulWidget {
   @override
-  ToDoListState createState() => ToDoListState();
+  _To_DO_ListState createState() => _To_DO_ListState();
 }
 
-class ToDoListState extends State<ToDoList> {
+class _To_DO_ListState extends State<To_DO_List> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _loadTodosFromServer(); 
-    });
-  }
-
-  void _loadTodosFromServer() async {
-    final provid = Provider.of<Todo>(context, listen: false);
-    List<Todo> todosFromServer = await getFromServer(); 
-    provid.updateList(todosFromServer); // Update the list in the Todo model
+    Provider.of<TodoListModel>(context, listen: false).loadTodosFromServer();
   }
 
   @override
   Widget build(BuildContext context) {
-    final provid = Provider.of<Todo>(context);
+    final provid = Provider.of<TodoListModel>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("TIG333 TODO"),
@@ -66,44 +51,37 @@ class ToDoListState extends State<ToDoList> {
           })
         ],
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-                itemCount: provid.getList.length,
-                itemBuilder: (context, index) {
-                  final item = provid.getList[index];
-                  return ListTile(
-                    leading: Checkbox(
-                      value: item.checked,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          provid.getList[index].checked = value!;
-                          provid.uppdateTodO(value, item.id, item.uppdrag); // Corrected method name
-                        });
-                      },
-                    ),
-                    title: Text(
-                      item.uppdrag,
-                      style: TextStyle(
-                          decoration: item.checked
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none),
-                    ),
-                    trailing: IconButton(
-                        onPressed: () {
-                          provid.deleteTodo(item.id); // Delete item
-                        },
-                        icon: Icon(Icons.cancel)),
-                  );
-                }),
-          )
-        ],
+      body: ListView.builder(
+        itemCount: provid.getList.length,
+        itemBuilder: (context, index) {
+          final item = provid.getList[index];
+          return ListTile(
+            leading: Checkbox(
+              value: item.checked,
+              onChanged: (bool? value) {
+                provid.updateTodo(value!, item.id, item.uppdrag);
+              },
+            ),
+            title: Text(
+              item.uppdrag,
+              style: TextStyle(
+                decoration: item.checked
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {
+                provid.removeFromList(item.id);
+              },
+              icon: Icon(Icons.cancel),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddToList()));
+          Navigator.push(context, MaterialPageRoute(builder: (context) => AddToList()));
         },
         child: Icon(Icons.add),
       ),
